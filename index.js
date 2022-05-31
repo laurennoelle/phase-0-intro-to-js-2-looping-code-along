8c54f90e6ce27990c36b0ac951d5bb88fb432268
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-05-30 21:51:48 -0600

    commit tree 95a646eaf533218c43167a4655600defbffd0f5f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name, events) {
+  const messages = [ ];
+  for(let i = 0; i < name.length; i++) {
+    let events = `Thank you, ${name[i]}, for the wonderful surprise gift!`
+    messages.push(events)
+  }
+  return messages;
+}
+
+
+function countDown(i) {
+  while(i >= 0) {
+    console.log(i);
+    i-=1;
+  }
+}
